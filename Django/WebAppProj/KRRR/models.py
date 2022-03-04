06283390b476cf56d9ff4f1b7b225eb132ea81c8
DP-00 <pasiak.dagmara@gmail.com>
2022-03-04 15:49:28 +0100

    adding folder for html and css files in django

--- Django/WebAppProj/KRRR/models.py
+++ Django/WebAppProj/KRRR/models.py
@@ -1,3 +1,18 @@
+#every time model is changed run:
+#>python manage.py makemigrations appName
+
+#command takes migration names and returns their SQL (doesn’t actually run the migration on your database - instead, it prints it to the screen so that you can see what SQL Django):
+#>python manage.py sqlmigrate appName 0001
+
+#to create those model tables in your database (run every time the model is changed)
+#>python manage.py migrate
+
 from django.db import models
 
-# Create your models here.
+class className(models.Model):
+	someText = models.CharField(max_length = 256)
+	someDate = models.DateTimeField('date published') #first parameter is defining human readable naem
+	someInt = models.IntegerField(default=0)
+	def __str__(self):
+        	return self.some_text  # text which will represent the object
+
