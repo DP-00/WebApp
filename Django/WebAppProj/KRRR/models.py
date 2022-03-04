8250a5157922e8a528e6bf59a50958b0fedac1c0
DP-00 <pasiak.dagmara@gmail.com>
2022-03-04 17:19:19 +0100

    add models

--- Django/WebAppProj/KRRR/models.py
+++ Django/WebAppProj/KRRR/models.py
@@ -8,11 +8,43 @@
 #>python manage.py migrate
 
 from django.db import models
+from datetime import datetime
 
-class className(models.Model):
-	someText = models.CharField(max_length = 256)
-	someDate = models.DateTimeField('date published') #first parameter is defining human readable naem
-	someInt = models.IntegerField(default=0)
-	def __str__(self):
-        	return self.some_text  # text which will represent the object
+class Products(models.Model):
+    label = models.CharField(max_length = 50)
+    price = models.FloatField
+    description = models.CharField(max_length = 250)
+    photo = models.BinaryField
+    sale_price = models.FloatField
 
+class Customers(models.Model):
+    name = models.CharField(max_length = 50)
+    surname = models.CharField(max_length = 50)
+    nickname = models.CharField(max_length = 50)
+    email = models.CharField(max_length = 100)
+    password = models.CharField(max_length = 50)
+
+class Orders(models.Model):
+    LOCATION_TYPE=(
+        ('city', 'city centre'),
+        ('MOA', 'AMFI Moa'),
+    )
+    customer = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Products)
+    order_date = models.DateTimeField(default=datetime.now())
+    location = models.CharField(max_length = 4, choices=LOCATION_TYPE)
+
+class Comments(models.Model):
+    STARS_TYPE=(
+        ('1', 'Very Bad'),
+        ('2', 'Bad'),
+        ('3', 'Neutral'),
+        ('4', 'Positive'),
+        ('5', 'Excelant'),
+    )
+    customer = models.ForeignKey(Customers, on_delete=models.CASCADE)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE)
+    comment_date = models.DateTimeField(default=datetime.now())
+    stars = models.CharField(max_length=1, choices=STARS_TYPE, default='5')
+    content = models.CharField(max_length = 1000, blank=True)
+    
