5af1b5ec0a23059397cd59501c16eaa41ecfd7c3
Marcelina <marcelinasuszczyk@gmail.com>
2022-03-13 17:30:29 +0100

    Add paths to the views

--- Django/WebAppProj/KRRR/urls.py
+++ Django/WebAppProj/KRRR/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name="index"),
+    path('shop/', views.shop, name="shop"),
+    path('cart/', views.cart, name="cart"),
+    path('checkout/', views.checkout, name="checkout")
 ]
--- Django/WebAppProj/KRRR/views.py
+++ Django/WebAppProj/KRRR/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
 def index(request):
-    return HttpResponse("Hello, world. You're at the app index.")
+    context = {}
+    return render(request, 'KRRR/index.html', context)
+
+def shop(request):
+    context = {}
+    return render(request, 'KRRR/shop.html', context)
+
+def cart(request):
+    context = {}
+    return render(request, 'KRRR/cart.html', context)
+
+def checkout(request):
+    context = {}
+    return render(request, 'KRRR/checkout.html', context)
