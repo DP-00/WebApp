8e8b98a201c5fb71c8f76df7a81a3c3561821f9f
Szymon Sciegienny <szymon.m.sciegienny@gmail.com>
2022-04-22 12:21:23 +0200

    Adding new urls

--- Django/WebAppProj/KRRR/urls.py
+++ Django/WebAppProj/KRRR/urls.py
@@ -27,7 +27,25 @@ urlpatterns = [
 
 # admin urls
     path('admin-admin/', views.adminAdmin, name="admin-admin"),
-    path('admin-users/', views.adminUsers, name="admin-users"),
+
+    path('admin-users/', views.AdminUserListView.as_view(), name="admin-users"),
+    path('admin-users/<int:pk>/', views.AdminUserView.as_view(), name='admin-user'),
+    path('admin-users/<int:pk>/delete/', views.AdminUserDeleteView.as_view(), name='user-delete'),
+    path('admin-users/<str:username>/orders/', views.AdminUserOrderListView.as_view(), name='admin-user-orders'),
+    path('admin-users/<str:username>/comments/', views.AdminUserCommentListView.as_view(), name='admin-user-comments'),
+
+    path('admin-products/', views.AdminProductListView.as_view(), name="admin-products"),
+    path('admin-products/create/', views.AdminProductCreatetView.as_view(), name="product-create"),
+    path('admin-products/<int:pk>/', views.AdminProductView.as_view(), name='admin-product'),
+    path('admin-products/<int:pk>/delete/', views.AdminProductDeleteView.as_view(), name='product-delete'),
+
+    path('admin-orders/', views.AdminOrderListView.as_view(), name="admin-orders"),
+    path('admin-orders/<int:pk>/', views.AdminOrderView.as_view(), name='admin-order'),
+    path('admin-orders/<int:pk>/delete/', views.AdminOrderDeleteView.as_view(), name='order-delete'),
+
+    path('admin-comments/', views.AdminCommentListView.as_view(), name="admin-comments"),
+    path('admin-comments/<int:pk>/', views.AdminCommentView.as_view(), name='admin-comment'),
+    path('admin-comments/<int:pk>/delete/', views.AdminCommentDeleteView.as_view(), name='comment-delete'),
 
 # credits
     path('credits/', views.credits, name="credits"),
