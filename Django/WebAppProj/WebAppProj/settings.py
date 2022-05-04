c51f1dccc6d5adc858dce48cb07e4fac13f522ac
Marcelina <marcelinasuszczyk@gmail.com>
2022-05-04 20:48:02 +0200

    media settings

--- Django/WebAppProj/WebAppProj/settings.py
+++ Django/WebAppProj/WebAppProj/settings.py
@@ -61,6 +61,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.template.context_processors.media',
 ]
 
 ROOT_URLCONF = 'WebAppProj.urls'
