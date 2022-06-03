1d76d7cbb9a48dc3948873b248cd48f0edcd86ad
DP-00 <57316056+DP-00@users.noreply.github.com>
2022-06-03 21:53:05 +0200

    commenting hero

--- Django/WebAppProj/static/js/hero.js
+++ Django/WebAppProj/static/js/hero.js
@@ -1,8 +1,16 @@
+// function to change text in the hero section
+
 let i = 0;
 function heroText(){
   let text=["modern", "healthy", "urban", "individual", "free", "hipster", "authentic", "modern", "modern", "modern", "modern"];
   let textLen = text.length;
+  
+  // looping over the array using modulo and increasing counter 
+  
   document.getElementById('heroText').innerText = text[i%textLen];
   i++;            
 }
+
+// text changes every 500 milliseconds
+
 setInterval(heroText, 500);
